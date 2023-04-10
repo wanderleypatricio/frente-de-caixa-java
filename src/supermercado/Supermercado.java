939096dcd5fbe8507a09b1920dec96436fddb51f
Wanderley Patrício <wpatricio26@gmail.com>
2023-04-10 13:45:58 -0300

    envio do projeto

---
+++ src/supermercado/Supermercado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supermercado;
+
+/**
+ *
+ * @author Patrício
+ */
+public class Supermercado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrenteCaixa fc = new FrenteCaixa();
+        fc.show();
+    }
+    
+}
